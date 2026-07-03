9ae152e11323b6e61aed430f903b445179d768cb
Adarsh Pahare <pahareadarsh@gmail.com>
2026-07-03 12:31:20 +0530

    add test cases

--- learning_jenkins/controllers/add.js
+++ learning_jenkins/controllers/add.js
@@ -1,10 +1,14 @@
+const { add } = require("./calculator");
 
+const add = (req, res) => {
 
- const add =(req,res)=>{
-  const num1 = parseInt(req.query.num1);
-  const num2 = parseInt(req.query.num2);
-  const sum = num1 + num2;
-  res.send(`The sum of ${num1} and ${num2} is ${sum}`); 
+    const num1 = Number(req.query.num1);
+    const num2 = Number(req.query.num2);
+
+    const result = add(num1, num2);
+
+    res.send(`The sum of ${num1} and ${num2} is ${result}`);
+
 }
 
 module.exports = add;
